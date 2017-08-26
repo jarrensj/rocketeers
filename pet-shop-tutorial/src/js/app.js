3e28398b0f22ef91dfbc7f4d0f0be0ee3ee1dcfb
Jarren San Jose <sanjose.jarren@gmail.com>
2017-08-26 11:25:00 -0700

    remove old contracts, migration

--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -24,10 +24,16 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+    
     return App.initContract();
   },
 
